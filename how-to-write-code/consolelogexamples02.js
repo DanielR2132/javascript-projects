44635476b8b9903f58f15a23ef17db2d124df8a3
DanielR2132 <154104609+DanielR2132@users.noreply.github.com>
2024-01-10 14:55:46 -0600

    Update consolelogexamples02.js

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -17,3 +17,21 @@ console.log(Number(33));
 console.log(Number("3 3"));
 console.log(Number(3));
 
+console.log(Boolean("true"));
+console.log(Boolean("TRUE"));
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(''));
+console.log(Boolean('LaunchCode'));
+
+console.log(Boolean(4 == "4"))
+
+let num = 7;
+
+if (num % 2 === 0) {
+    console.log("EVEN");
+
+    if (num > 0) {
+        console.log("POSITIVE");
+    }
+}
