0e7454144eca1b7ea534791a4109f577a910428d
DanielR2132 <154104609+DanielR2132@users.noreply.github.com>
2024-01-11 09:46:53 -0600

    Update data-and-variables-exercises.js

--- how-to-write-code/data-and-variables-exercises.js
+++ how-to-write-code/data-and-variables-exercises.js
@@ -23,3 +23,19 @@ let daystomars = hoursToMars / 24
 
 console.log(spaceshuttle + " will take " + daystomars + " days to reach the moon");
 
+console.log(4 == "4")
+
+let a = 7;
+if (a % 2 === 1) {
+   console.log("Launch");
+} else if (a > 5) {
+   console.log("Code");
+} else {
+   console.log("LaunchCode");
+}
+let num = 1;
+if (num % 2 === 0) {
+    if (num % 2 === 1) {
+        console.log("odd");
+    }
+}
