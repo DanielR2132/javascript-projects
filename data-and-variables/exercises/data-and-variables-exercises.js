c3609fb5fe1decf54f9f401832efcfacfc966636
DanielR2132 <154104609+DanielR2132@users.noreply.github.com>
2024-01-11 20:33:54 -0600

    update

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,8 +1,12 @@
 // Declare and assign the variables below
 
+
 // Use console.log to print the 'typeof' each variable. Print one item per line.
 
+
 // Calculate a space mission below
+
+
 
 // Print the results of the space mission calculations below
 
